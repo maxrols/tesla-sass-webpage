16a28b5d99e35fd80fad72e6bbba2b4f2655f304
Max <example@gmail.com>
2022-01-28 19:19:02 +0100

    Added main block. Not adaptive

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const listMenu = document.querySelector(".header__menu"),
-          menuItems = document.querySelectorAll('.menu__item')
+    const imgs = ['img/car1.png', 'img/car2.png'];
+
+    let listMenu = document.querySelector(".header__menu"),
+          menuItems = document.querySelectorAll('.menu__item'),
+          car = document.querySelector('.main-block__image img');
 
     function removeActive() {
         menuItems.forEach((item) => {
@@ -11,18 +14,25 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    function getActive(i = 0) {
-        menuItems[i].classList.add('menu__item_active');
+    function getActive(menu, i = 0) {
+        menu[i].classList.add('menu__item_active');
+    }
+
+    function changeCar(i) {
+        car.src = imgs[i];
+        document.querySelector('.main-block__title').textContent = menuItems[i].textContent;
     }
     removeActive();
-    getActive();
+    getActive(menuItems);
     listMenu.addEventListener('click', (event) => {
         let listItem = event.target;
         menuItems.forEach((el, i) => {
             if(el == listItem) {
                 removeActive();
-                getActive(i);
+                getActive(menuItems, i);
+                changeCar(i);
             }
         });
+        
     });
 });
